02c4c40b0181ace87795a396b67beb52a1575e25
JP <jp@beyondlogical.com>
2020-12-31 13:24:51 -0500

    first commit

---
+++ LighOnOffWebsocket.ino
@@ -0,0 +1,87 @@
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+const char* ssid = "Fios-WVBRA_EXT";
+const char* password = "pea4879carve43tony";
+const char* websocket_server_host = "192.82.251.138";
+const uint16_t websocket_server_port = 8888;
+const int relay = 26;
+
+using namespace websockets;
+WebsocketsClient client;
+void onMessageCallback(WebsocketsMessage message) {
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+    if(message.data() == "hey hi ho"){
+      digitalWrite(relay, LOW);
+    } else{
+      digitalWrite(relay, HIGH);
+    }
+
+}
+
+void connect(){
+//  client.onMessage(onMessageCallback);
+//  client.onEvent(onEventsCallback);
+    
+  while(!client.connect(websocket_server_host, websocket_server_port, "/")){
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("Websocket Connected!");
+  
+  Serial.begin(115200);
+}
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("ConnectionOpened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("ConnectionClosed");
+        connect();
+    } else if(event == WebsocketsEvent::GotPong) {
+        Serial.println("GotPong");
+    }
+}
+
+void setup() {
+
+   WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  client.onMessage(onMessageCallback);
+  client.onEvent(onEventsCallback);
+//    
+//  while(!client.connect(websocket_server_host, websocket_server_port, "/")){
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  
+//  Serial.println("Websocket Connected!");
+//  
+//  Serial.begin(115200);
+  connect();
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+   client.poll();
+//  // Normally Open configuration, send LOW signal to let current flow
+//  // (if you're usong Normally Closed configuration send HIGH signal)
+//  digitalWrite(relay, LOW);
+//  Serial.println("Current Flowing");
+//  delay(5000); 
+//  
+//  // Normally Open configuration, send HIGH signal stop current flow
+//  // (if you're usong Normally Closed configuration send LOW signal)
+//  digitalWrite(relay, HIGH);
+//  Serial.println("Current not Flowing");
+//  delay(5000);
+}
