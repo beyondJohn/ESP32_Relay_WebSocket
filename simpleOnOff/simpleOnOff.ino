4fba60a817461fc03b07f2bbde70a901adc900e5
beyondjohn <jpjpiesco@gmail.com>
2021-03-31 23:59:57 -0400

    added simpleOnOff switch sketch

---
+++ simpleOnOff/simpleOnOff.ino
@@ -0,0 +1,97 @@
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Fios-WVBRA_EXT";
+const char* password = "pea4879carve43tony";
+//const char* ssid = "Pixel_6944";
+//const char* password = "jphotspot";
+const char* websocket_server_host = "192.82.251.138";
+const uint16_t websocket_server_port = 8888;
+const int relay = 26;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+//unsigned long timerDelay = 600000;
+unsigned long timerDelay = 5000;
+
+using namespace websockets;
+WebsocketsClient client;
+
+void onMessageCallback(WebsocketsMessage message) {
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+    if(message.data() == "plugOn"){
+      digitalWrite(relay, HIGH);
+    }
+    else if(message.data() == "plugOff"){
+      digitalWrite(relay, LOW);
+    } 
+}
+
+void connect(){
+    
+  while(!client.connect(websocket_server_host, websocket_server_port, "/")){
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("Websocket Connected!");
+
+}
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("ConnectionOpened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("ConnectionClosed");
+        delay(1000);
+        connect();
+    } else if(event == WebsocketsEvent::GotPong) {
+        Serial.println("GotPong");
+    }
+}
+
+void Wifi(){
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+void setup() {
+  Serial.begin(115200);
+  Wifi();
+  client.onMessage(onMessageCallback);
+  client.onEvent(onEventsCallback);
+  pinMode(relay, OUTPUT);
+  connect();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  client.poll();
+   if(client.available() != 1){
+    Serial.print(client.available()); 
+    if(WiFi.status() != WL_CONNECTED){
+      Wifi();
+    }
+    connect();
+   }
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+  
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+
+}
